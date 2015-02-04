96124dbbfbe748029b630e2e261b8a1388790dcb
Zachary Meadows <zacmeadows@me.com>
2015-02-03 19:13:52 -0800

    adding first name and last name to registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    if first_name || last_name
+      "#{first_name} #{last_name}".squeeze.strip
+    else
+      email
+    end
+  end
+
+
 end
